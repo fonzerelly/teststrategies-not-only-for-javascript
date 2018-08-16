5db1f0acc7445250137c041380250dbc1cc01cfd
cs8433 <christian.hoerauf@consorsbank.de>
2018-08-16 12:31:10 +0200

    support proper code highlighting

--- release.sh
+++ release.sh
@@ -4,6 +4,7 @@ npm version minor
 git checkout gh-pages
 git merge master
 sed -i -- 's/node_modules\/reveal\.js/https:\/\/cdnjs.cloudflare.com\/ajax\/libs\/reveal.js\/3.6.0/g' index.html
+sed -i -- 's/node_modules\/highlight\.js/https:\/\/cdnjs.cloudflare.com\/ajax\/libs\/highlight.js\/9.12.0/g' index.html
 git add index.html
 git commit -m "Auto: replace node_modules with CDN"
 git push origin gh-pages
